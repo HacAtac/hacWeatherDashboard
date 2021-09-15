e2d82e2ec689b027f69c1552ea12c1b933e86b81
HacAtac <jhack00@icloud.com>
2021-09-15 16:00:22 -0500

    added currentCondition function and added event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,63 @@
 // Access to OpenWeather Api
-var ApiKey = "11844fc65296129b6cf4bbaa841fc2e6";
-var queryUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon{lon}" + ApiKey;
-var searchHistoryList = document.querySelector("#search-list");
-var searchCity = document.querySelector("#search");
-var searchCityBtn = document.querySelector("#search-button")
+var apiKey = "11844fc65296129b6cf4bbaa841fc2e6";
+//will store searched cities in this.
+var citySearchList = [];
+//moment.js for current day
+var today = moment().format('l');
+document.getElementById("#search-list");
+
+
+// Need to make a function that will show current conditions 
+// Will show conditions, temp, humidity, wind, UV index, also need font awesome icon for current condition
+// Current Condition UV index should show favorable, moderate, or severe color
+function currentConditions(city) {
+    var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(cityResponse){
+        console.log(cityResponse);
+        $("#cityInfo").empty();
+
+        var iconPic = cityResponse.weather[0].icon;
+        var iconURL = `https://openweathermap.org/img/w/${iconPic}.png`;
+        var currentCity = $(`
+        <h2 id= "currentCity">
+            ${cityResponse.name} ${today} <img src="${iconURL}" alt="${cityResponse.weather[0].description}" /></h2>
+        <p>Temp: ${cityResponse.main.temp} F</p>
+        <p>Humidity: ${cityResponse.main.humidity}%</p>
+        <p>Wind: ${cityResponse.wind.speed}MPH</p>
+            `);
+    $("#cityInfo").append(currentCity);
+        
+    })
+
+}
+
+// And also it adds to the search history NEED THE SEARCHES TO DISPLAY ON PAGE
+
+
+
+// Need to make a function that will show next conditions within 5 days
+// Will need to show conditions, temp, humidity, wind, UV index for the next 5 days.
+// When you view the UV index needs to show a fontawesome icon regarding what condition it is
+
+
+
+//click event listener for the search button
+$("#search-button").on("click", function(event) {
+    event.preventDefault();
+    var city = $("#search").val().trim();
+    currentConditions(city);
+    if (!citySearchList.includes(city)) {
+        citySearchList.push(city);
+        var cityInput = $(` <li class = "list-group-item">${city}</li>`);
+        $("#search").append(cityInput);
+    };
+    localStorage.setItem("searched-city", JSON.stringify(citySearchList));
+    console.log(citySearchList);
+})
+
+
+
